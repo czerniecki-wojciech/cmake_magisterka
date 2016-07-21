48a4e12fc9b8c611b77975261d361b817795ab36
Czerniecki Wojciech <czerniecki.wojciech@gmail.com>
2016-07-21 09:08:25 +0200

    wc

--- boost__example/test/main.cpp
+++ boost__example/test/main.cpp
@@ -1,14 +1,10 @@
-// example000_google.cpp : Defines the entry point for the console application.
-//
+
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Hello
 #include <boost\test\unit_test.hpp>
 
-#include "../src/simple_calc.h"
-
 BOOST_AUTO_TEST_CASE(TC1)
 {
-    simple_calc sc;
-    BOOST_CHECK_EQUAL(3, sc.addInts(1, 2));
+	
 }
 
